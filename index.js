eed2d11f80db78cee63d9fecde89df29866fa445
Andrew Sprague <andrewsprague@outlook.com>
2018-08-02 02:48:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-function shout()
+function shout(x) {
+  return x.toUpperCase();
+}
+
+function whisper(x) {
+  return x.toLowerCase();
+}
+
+function logShout (x) {
+  console.log(x.toUpperCase());
+}
+
+function logWhisper(x) {
+  console.log(x.toLowerCase());
+}
+
+function sayHiToGrandMa(x) {
+  if (x === x.toLowerCase()) {
+    return "I can't hear you!"
+  } else if (x === x.toUpperCase) {
+    return "YES INDEED!"
+  } else if ()
+}
+
+function sayHiToGrandMa (x) {
+  if  {
+    
+  }
+}
+
+function 
